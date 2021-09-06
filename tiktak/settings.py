4b08890721112293313aa9056219920ae0248334
phqlong <phqlong2810@gmail.com>
2021-09-06 22:23:29 +0700

    fix project root

--- tiktak/settings.py
+++ tiktak/settings.py
@@ -190,7 +190,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
